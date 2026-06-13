e6adfd18054083531b4f5557a5d82214ee69946b
Sidharth Sehrawat <sid2001sehrawat@gmail.com>
2026-06-13 21:41:41 +0530

    Fix vote redis DNS

--- example-voting-app/vote/app.py
+++ example-voting-app/vote/app.py
@@ -18,8 +18,13 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        g.redis = Redis(
+            host="redis.redis.svc.cluster.local",
+            db=0,
+            socket_timeout=5
+        )
     return g.redis
+
 
 @app.route("/", methods=['POST','GET'])
 def hello():
